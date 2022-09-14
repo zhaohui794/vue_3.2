882da43e8d749edbd35b360d6baa5976bc5d6148
是贝贝贝贝贝波a <1718297810@qq.com>
2022-09-14 23:18:56 +0800

    styles configuration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-=======
+
 import './plugins/axios'
->>>>>>> 62ecf9e (merge)
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
