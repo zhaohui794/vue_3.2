bfb3af11fe8cae941776b7bcffd6bf6f7a465709
是贝贝贝贝贝波a <1718297810@qq.com>
2022-09-24 23:53:33 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,23 @@
 <template>
-  <div class="login-container">login</div>
+  <div class="login-container">
+    <el-form ref="form" :model="form">
+      <el-form-item>
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const form = ref({
+  name: ''
+})
+</script>
 
 <style lang="scss" scoped>
 .login-container {
