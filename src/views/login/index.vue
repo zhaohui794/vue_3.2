5bf62179f0d74798e1a8400bd4cc5853b23dbfe1
是贝贝贝贝贝波a <1718297810@qq.com>
2022-09-25 23:36:55 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="login-container">
     <el-form ref="form" :model="form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
       <el-form-item>
-        <el-input v-model="form.name"></el-input>
+        <i class="el-icon-edit-outline" size="20"></i>
+        <el-input v-model="form.name" placeholder="请输入账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <i class="el-icon-edit-outline" size="20"></i>
+        <el-input
+          v-model="form.name"
+          show-password
+          placeholder="请输入密码"
+        ></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">立即创建</el-button>
